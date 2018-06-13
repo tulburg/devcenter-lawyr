9bfe66dc2aadddb4812e65cb2a2e2fb31ff2bf65
Tolu Oluwagbemi <tulburg2@gmail.com>
2018-06-14 00:33:34 +0100

    homepage particles

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -14,6 +14,34 @@
 		<section class="home" id="home">
 			<div class="container main-container">
 				<div class="bg-box"></div>
+				<div class="text-container">
+					<h1>An Easy way for an Easy A</h1>
+					<p>Made for Law Students, Lawyr helps you study for your Law School examinations with nothing but your mobile phone.</p>
+					<button class="alt">Sign Up</button> <button class="bordered">Learn More</button>
+				</div>
+				<div class="image-container">
+					<img class="gavel" src="../assets/img/hammer.png" alt="gavel" />
+					<img class="phone-2" src="../assets/img/phone_1.png" alt="IPhone X" />
+					<img class="phone-1" src="../assets/img/phone_2.png" alt="IPhone X Too" />
+				</div>
+			</div>
+		</section>
+		<section class="tests">
+			<div class="container">
+				<div class="text-container">
+					<div class="xbadge">Tests</div>
+					<h1>An Easy way for an Easy A</h1>
+					<p>Made for Law Students, Lawyr helps you study for your Law School examinations with nothing but your mobile phone.</p>
+				</div>
+				<div class="image-container">
+					<img class="curves" src="../assets/img/background_curve.svg" alt="curves" />
+					<div class="particles">
+						<span class="part-one"></span>
+						<span class="part-two"></span>
+						<span class="part-three"></span>
+						<span class="part-four"></span>
+					</div>
+				</div>
 			</div>
 		</section>
 	</div>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+window.Array.prototype.where = function(obj) {
+	for(var i=0;i<this.length;i++) {
+		for(var v in obj) {
+			if(this[i][v] && this[i][v] === obj[v]) return this[i];
+		}
+	}
+	return undefined;
+}
 window.Element.prototype.$ = window.$ = function(id) {
 	if(id[0] == ".") return (this) ? (this.getElementsByClassName(id.substring(1)).length == 1) ? this.getElementsByClassName(id.substring(1))[0] : this.getElementsByClassName(id.substring(1)) : (document.getElementsByClassName(id.substring(1)).length == 1) ? document.getElementsByClassName(id.substring(1))[0] : document.getElementsByClassName(id.substring(1));
 	if(id[0] == "#") return (this) ? this.getElementById(id.substring(1)) : document.getElementById(id.substring(1));
